5dc7100386a87877a2a13f7f33dd444cf6d09219
Lama13372002 <130605808+Lama13372002@users.noreply.github.com>
2025-07-03 15:33:41 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
             key: 'X-Frame-Options',
